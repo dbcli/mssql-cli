0df5cb4943630d546bd26351857409c24ffc86c1
abhisabh <abhisabh@microsoft.com>
2017-12-04 16:55:29 -0800

    update dev_setup

--- dev_setup.py
+++ dev_setup.py
@@ -10,7 +10,7 @@ utility.exec_command('pip install -r requirements-dev.txt', utility.ROOT_DIR)
 
 platform = utility.get_current_platform()
 if platform != 'win32' and platform != 'win_amd64':
-    utility.exec_command('pip install setproctitle>=1.1.9')
+    utility.exec_command('pip install setproctitle>=1.1.9', utility.ROOT_DIR)
 
 # install mssqltoolsservice if this platform supports it.
 utility.copy_current_platform_mssqltoolsservice()
