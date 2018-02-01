9f9ba77417a37fbd6e70dbf3bac08176e3d01b30
abhisabh <abhisabh@microsoft.com>
2018-01-31 16:58:29 -0800

    Add list schemas special command

--- mssqlcli/packages/special/commands.py
+++ mssqlcli/packages/special/commands.py
@@ -4,7 +4,7 @@ from .main import special_command, RAW_QUERY, PARSED_QUERY, NO_QUERY
 logger = logging.getLogger('mssqlcli.commands')
 
 
-@special_command('\\l', '\\l[+] [pattern]', 'List databases', aliases=('\\list',))
+@special_command('\\l', '\\l[+] [pattern]', 'List databases.', aliases=('\\list',))
 def list_databases(mssqlcliclient, pattern, verbose):
     base_query = u'select {0} from sys.databases'
     if verbose:
@@ -15,3 +15,18 @@ def list_databases(mssqlcliclient, pattern, verbose):
         base_query += " where name like '%{0}%'".format(pattern)
 
     return mssqlcliclient.execute_multi_statement_single_batch(base_query)
+
+
+@special_command('\\dn', '\\dn[+] [pattern]', 'List schemas.')
+def list_schemas(mssqlcliclient, pattern, verbose):
+    base_query = u'select {0} from sys.schemas'
+    if verbose:
+        base_query = base_query.format('name, schema_id, principal_id')
+    else:
+        base_query = base_query.format('name')
+    if pattern:
+        base_query += " where name like '%{0}%'".format(pattern)
+
+    return mssqlcliclient.execute_multi_statement_single_batch(base_query)
+
+
