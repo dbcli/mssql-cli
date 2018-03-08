3d53992840832f6222f4c07401662015a55d29ea
MrMeemus <rquan07@gmail.com>
2018-03-08 11:15:55 -0500

    Updating debian to only build with python3 since the package is frozen with python3.

--- build_scripts/debian/dir_creator.sh
+++ build_scripts/debian/dir_creator.sh
@@ -78,7 +78,7 @@ cat > $debian_dir/rules << EOM
 #export DH_OPTIONS=-v
 
 %:
-${TAB}dh \$@ --sourcedirectory $source_dir
+${TAB}dh \$@ --sourcedirectory $source_dir --with python3 --buildsystem=pybuild
 
 override_dh_install:
 ${TAB}mkdir -p debian/mssql-cli/mssql-cli
@@ -87,7 +87,6 @@ ${TAB}mkdir -p debian/mssql-cli/usr/bin/
 ${TAB}echo "if [ -z ${PYTHONIOENCODING+x} ]; then export PYTHONIOENCODING=utf8; fi" > debian/mssql-cli/usr/bin/mssql-cli
 ${TAB}echo "\043!/usr/bin/env bash\n/mssql-cli/main \"\044\100\"" > debian/mssql-cli/usr/bin/mssql-cli
 ${TAB}chmod 0755 debian/mssql-cli/usr/bin/mssql-cli
-${TAB}dpkg-shlibdeps -v --warnings=7 -Tdebian/mssql-cli.substvars -dDepends -edebian/mssql-cli/mssql-cli/bin/python3 debian/mssql-cli/mssql-cli/lib/python3.6/lib-dynload/_ssl.cpython-36m-x86_64-linux-gnu.so
 
 
 override_dh_strip:
