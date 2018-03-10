55e37d07eecb7c5a2cd1b1552cdc12675e818532
Ronald Quan <rquan07@gmail.com>
2018-03-09 19:07:36 -0500

    Updating dev setup to not install from cache dir. (#176)

--- dev_setup.py
+++ dev_setup.py
@@ -6,7 +6,7 @@ print('Running dev setup...')
 print('Root directory \'{}\'\n'.format(utility.ROOT_DIR))
 
 # install general requirements.
-utility.exec_command('pip install -r requirements-dev.txt', utility.ROOT_DIR)
+utility.exec_command('pip install --no-cache-dir -r requirements-dev.txt', utility.ROOT_DIR)
 
 # install mssqltoolsservice if this platform supports it.
 utility.copy_current_platform_mssqltoolsservice()
