cb107d672833ca8c7da0e20dc15c51487e5ebafa
MrMeemus <rquan07@gmail.com>
2018-03-26 10:31:03 -0700

    Removing pybuild argument override for dh.

--- build_scripts/debian/dir_creator.sh
+++ build_scripts/debian/dir_creator.sh
@@ -78,7 +78,7 @@ cat > $debian_dir/rules << EOM
 #export DH_OPTIONS=-v
 
 %:
-${TAB}dh \$@ --sourcedirectory $source_dir --with python3.6 --buildsystem=pybuild
+${TAB}dh \$@ --sourcedirectory $source_dir
 
 override_dh_install:
 ${TAB}mkdir -p debian/mssql-cli/mssql-cli
