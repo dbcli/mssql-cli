2afcd46a6dc851ad0b544b092fa49a843f6ab506
MrMeemus <rquan07@gmail.com>
2018-04-02 14:07:46 -0700

    Update build drop path for rpm package.

--- build_scripts/rpm/build.sh
+++ build_scripts/rpm/build.sh
@@ -12,5 +12,5 @@ rpmbuild -v -bb --clean mssql-cli.spec
 
 # Copy build artifact to output dir.
 mkdir ${REPO_PATH}/rpm_output
-cp ~/rpmbuild/RPMS/x86_64/*.rpm ${REPO_PATH}/rpm_output
-echo "The archive has also been outputted to ${REPO_PATH}/rpm_output"
+cp ~/rpmbuild/RPMS/x86_64/*.rpm ${REPO_PATH}/../rpm_output
+echo "The archive has also been outputted to ${REPO_PATH}/../rpm_output"
