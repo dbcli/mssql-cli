8361b4bd18cdee8f579620bf3fc237452b221eb9
MrMeemus <rquan07@gmail.com>
2018-07-03 12:13:25 -0700

    Fixing connection summary decode when connection fails.

--- mssqlcli/jsonrpc/contracts/connectionservice.py
+++ mssqlcli/jsonrpc/contracts/connectionservice.py
@@ -137,7 +137,7 @@ class ConnectionCompleteEvent(object):
             self.server_edition = inner_params[u'serverInfo'][u'serverEdition'] if \
                 u'serverEdition' in inner_params[u'serverInfo'] else None
 
-        if u'connectionSummary' in inner_params and inner_params[u'connectionSummary'][u'databaseName']:
+        if u'connectionSummary' in inner_params and inner_params[u'connectionSummary']:
             self.connected_database = inner_params[u'connectionSummary'][u'databaseName']
 
 
