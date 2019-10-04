dbea6797c7e22f5e37bbe2d14aaa8ed7b7231e7b
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-10-03 22:17:37 -0700

    removed placeholder for testing outputing of -Q

--- tests/test_noninteractive_mode.py
+++ tests/test_noninteractive_mode.py
@@ -1,35 +1,27 @@
 """ Non-interactive tests. """
 from mssqltestutils import create_mssql_cli
 
-class TestNonInteractiveMode:
-    """ Non-interactive tests. """
-    def test_session_closure_query_valid(self):
-        """ Test session closure for valid query. """
-        assert self.is_processed_closed("select 1")
+def test_session_closure_query_valid():
+    """ Test session closure for valid query. """
+    assert is_processed_closed("select 1")
 
-    def test_session_closure_query_invalid(self):
-        """ Test session closure for invalid query. """
-        assert self.is_processed_closed("asdfasoifjas")
+def test_session_closure_query_invalid():
+    """ Test session closure for invalid query. """
+    assert is_processed_closed("asdfasoifjas")
 
-    def test_execute_query(self):
-        """ Tests if -Q has equal output to execute_query. """
-        pass
+def is_processed_closed(query_str):
+    """ Runs unit tests on process closure given a query string """
+    mssql_cli = create_mssql_cli(query=query_str)
+    mssql_cli.connect_to_database()
 
-    @classmethod
-    def is_processed_closed(cls, query_str):
-        """ Runs unit tests on process closure given a query string """
-        mssql_cli = create_mssql_cli(query=query_str)
-        mssql_cli.connect_to_database()
+    # ensures process is still running before -Q is processed
+    if is_process_terminated(mssql_cli):
+        return False
+    mssql_cli.run()
+    # process should now be terminated
+    return is_process_terminated(mssql_cli)
 
-        # ensures process is still running before -Q is processed
-        if cls.is_process_terminated(mssql_cli):
-            return False
-        mssql_cli.run()
-        # process should now be terminated
-        return cls.is_process_terminated(mssql_cli)
-
-    @staticmethod
-    def is_process_terminated(mssql_cli):
-        """ Checks if mssql_cli instance has terminated. """
-        return mssql_cli.mssqlcliclient_main.sql_tools_client.tools_service_process.poll() \
-            is not None
+def is_process_terminated(mssql_cli):
+    """ Checks if mssql_cli instance has terminated. """
+    return mssql_cli.mssqlcliclient_main.sql_tools_client.tools_service_process.poll() \
+        is not None
