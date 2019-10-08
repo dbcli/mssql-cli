8ebab839c840c84eb6ee07d87dad1f5ad413feb1
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-10-08 15:58:24 -0700

    parent 2f7ff7393811dab928860d5a9226a406835ce233 author Elliot Boschwitz <elliot.boschwitz@gmail.com> 1570575504 -0700 committer Elliot Boschwitz <elliot.boschwitz@gmail.com> 1571865076 -0700
    beginning implementation for -o; merged changes from master

--- tests/test_noninteractive_mode.py
+++ tests/test_noninteractive_mode.py
@@ -12,6 +12,24 @@ class TestNonInteractiveResults:
     """
     Tests non-interactive features.
     """
+    @staticmethod
+    @pytest.fixture()
+    def tmp_filepath():
+        """ pytest fixture which returns filepath string and removes the file after tests
+        complete. """
+        fp = _FILE_ROOT + "tmp.txt"
+        yield fp
+        os.remove(fp)
+
+    @staticmethod
+    @pytest.fixture()
+    def tmp_filepath():
+        """ pytest fixture which returns filepath string and removes the file after tests
+        complete. """
+        fp = _FILE_ROOT + "tmp.txt"
+        yield fp
+        os.remove(fp)
+
     testdata = [
         ("SELECT 1", os.path.join(_FILE_ROOT, 'small.txt')),
         ("SELECT 1; SELECT 2;", os.path.join(_FILE_ROOT, 'multiple.txt')),
@@ -22,19 +40,32 @@ class TestNonInteractiveResults:
 
     @pytest.mark.parametrize("query_str, file_baseline", testdata)
     def test_query(self, query_str, file_baseline):
+        """ Tests -Q """
         output_query = self.execute_query_via_subprocess(query_str)
         output_baseline = self.get_file_contents(file_baseline)
         assert output_query == output_baseline
 
-    def test_long_query(self):
+    @pytest.mark.parametrize("query_str, file_baseline", testdata)
+    def test_output_file(self, query_str, file_baseline, tmp_filepath):
+        """ Tests -o (and ensures file overwrite works) """
+        self.execute_query_via_subprocess(query_str, output_file=tmp_filepath)
+        output_query = self.get_file_contents(tmp_filepath)
+        output_baseline = self.get_file_contents(file_baseline)
+        assert output_query == output_baseline
+
+    def test_long_query(self, tmp_filepath):
         """ Output large query using Python class instance. """
         query_str = """ALTER DATABASE keep_AdventureWorks2014 SET COMPATIBILITY_LEVEL = 130
                     SELECT * FROM STRING_SPLIT(REPLICATE(CAST('X,' AS VARCHAR(MAX)), 1024), ',')"""
         try:
-            mssqlcli = create_mssql_cli(interactive_mode=False)
+            mssqlcli = create_mssql_cli(interactive_mode=False, output_file=tmp_filepath)
             output_query = '\n'.join(mssqlcli.execute_query(query_str))
             output_baseline = self.get_file_contents(os.path.join(_FILE_ROOT, 'big.txt'))
             assert output_query == output_baseline
+
+            # test output to file
+            output_query_from_file = self.get_file_contents(tmp_filepath)
+            assert output_query_from_file == output_baseline
         finally:
             mssqlcli.shutdown()
 
@@ -51,20 +82,23 @@ class TestNonInteractiveResults:
             mssqlcli.shutdown()
 
     @staticmethod
-    def execute_query_via_subprocess(query_str):
+    def execute_query_via_subprocess(query_str, output_file=None):
         """ Helper method for running a query with -Q. """
-        p = subprocess.Popen(os.path.join(".", "mssql-cli -Q \"%s\"" % query_str), shell=True,
-                             stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT)
+        cli_call = os.path.join(".", "mssql-cli -Q \"%s\"" % query_str)
+        if output_file is not None:
+            cli_call += " -o %s" % output_file
+        p = subprocess.Popen(cli_call, shell=True, stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = p.communicate()[0].decode("utf-8").replace('\r', '')
-        return output
+        return output.strip()
 
     @staticmethod
     def get_file_contents(file_path):
         """ Get expected result from file. """
         try:
             with open(file_path, 'r', ) as f:
-                return f.read().replace('\r', '')   # remove string literals needed in python2
+                # remove string literals (needed in python2) and newlines
+                return f.read().replace('\r', '').strip()
         except OSError as e:
             raise e
 
