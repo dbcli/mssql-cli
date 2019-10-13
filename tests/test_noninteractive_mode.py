2f8d43655b8846ea0555ba24f7a935e54977d187
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-10-13 13:04:58 -0700

    added tests for db modification, more to come

--- tests/test_noninteractive_mode.py
+++ tests/test_noninteractive_mode.py
@@ -6,24 +6,24 @@ import os
 import pytest
 from mssqltestutils import (
     create_mssql_cli,
-    # create_test_db,
-    # clean_up_test_db,
+    create_test_db,
+    clean_up_test_db,
 )
 
+_FILE_ROOT = '%s/test_query_results/' % os.path.dirname(os.path.abspath(__file__))
 
 class TestNonInteractiveResults:
     """
     Tests non-interactive features.
     """
 
-    file_root = '%s/test_query_results/' % os.path.dirname(os.path.abspath(__file__))
     testdata = [
         ("SELECT * FROM STRING_SPLIT(REPLICATE(CAST('X,' AS VARCHAR(MAX)), 1024), ',')",
-         '%sbig.txt' % file_root),
-        ("SELECT 1", '%ssmall.txt' % file_root),
-        ("SELECT 1; SELECT 2;", '%smultiple.txt' % file_root),
-        ("SELECT %s" % ('x' * 250), '%scol_too_wide.txt' % file_root),
-        ("SELECT REPLICATE(CAST('X,' AS VARCHAR(MAX)), 1024)", '%scol_wide.txt' % file_root)
+         '%sbig.txt' % _FILE_ROOT),
+        ("SELECT 1", '%ssmall.txt' % _FILE_ROOT),
+        ("SELECT 1; SELECT 2;", '%smultiple.txt' % _FILE_ROOT),
+        ("SELECT %s" % ('x' * 250), '%scol_too_wide.txt' % _FILE_ROOT),
+        ("SELECT REPLICATE(CAST('X,' AS VARCHAR(MAX)), 1024)", '%scol_wide.txt' % _FILE_ROOT)
     ]
 
     @pytest.mark.parametrize("query_str, file_baseline", testdata)
@@ -63,6 +63,47 @@ class TestNonInteractiveResults:
             raise e
 
 
+class TestNonInteractiveDbModification:
+    """
+    Test DB modification in non-interactive mode.
+    """
+
+    @staticmethod
+    @pytest.fixture(scope="class")
+    def test_db():
+        """ pytest fixture that creates test db """
+        test_db = create_test_db()
+        yield test_db
+        clean_up_test_db(test_db)
+
+    @staticmethod
+    @pytest.fixture(scope="function")
+    def mssqlcli(test_db):
+        return create_mssql_cli(interactive_mode=False, database=test_db)
+
+    """
+    TODO: TESTS TO CONSIDER:
+    create table
+    modify table name
+    modify column
+    add column
+    delete column
+    delete table
+"""
+
+    @staticmethod
+    def test_create_table(test_db, mssqlcli):
+        """ Tests expected result for query. """
+        output = str(mssqlcli.execute_query("CREATE TABLE %s (test_col_int int)" % test_db))
+        assert output == "['Commands completed successfully.']"
+
+    @staticmethod
+    def test_drop_table(test_db, mssqlcli):
+        """ Tests expected result for query. """
+        output = str(mssqlcli.execute_query("DROP TABLE %s" % test_db))
+        assert output == "['Commands completed successfully.']"
+
+
 class TestNonInteractiveShutdown:
     """
     Ensures that client session has shut down after mssql-cli runs in non-interactive mode.
@@ -87,9 +128,3 @@ class TestNonInteractiveShutdown:
             self.mssql_cli.shutdown()
             assert self.mssql_cli.mssqlcliclient_main.sql_tools_client.\
                 tools_service_process.poll() is not None
-
-# TODO
-class TestNonInteractiveDbModification:
-    """
-    Test DB modification in non-interactive mode.
-    """
