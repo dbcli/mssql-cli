5e5a5790338bf8b05dabe137fd9ae222c3720421
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-11-22 16:08:21 -0800

    updated calls in test

--- tests/test_rowlimit.py
+++ tests/test_rowlimit.py
@@ -15,28 +15,31 @@ class RowLimitTests(unittest.TestCase):
 
     def test_default_row_limit(self):
         cli = MssqlCli(self.DEFAULT_OPTIONS)
-        result = cli._should_show_limit_prompt(['row']*self.low_count)
+        stmt = "SELECT * FROM students"
+        result = cli._should_show_limit_prompt(stmt, ['row']*self.low_count)
         assert not result
 
-        result = cli._should_show_limit_prompt(['row']*self.over_default)
+        result = cli._should_show_limit_prompt(stmt, ['row']*self.over_default)
         assert result
 
     def test_no_limit(self):
         cli_options = create_mssql_cli_options(row_limit=0)
         cli = MssqlCli(cli_options)
         assert cli.row_limit == 0
+        stmt = "SELECT * FROM students"
 
-        result = cli._should_show_limit_prompt(['row']*self.over_limit)
+        result = cli._should_show_limit_prompt(stmt, ['row']*self.over_limit)
         assert not result
 
     def test_row_limit_on_non_select(self):
         cli = MssqlCli(self.DEFAULT_OPTIONS)
-        result = cli._should_show_limit_prompt(None)
+        stmt = "UPDATE students set name='Boby'"
+        result = cli._should_show_limit_prompt(stmt, None)
         assert not result
 
         cli_options = create_mssql_cli_options(row_limit=0)
         assert cli_options.row_limit == 0
         cli = MssqlCli(cli_options)
-        result = cli._should_show_limit_prompt(['row']*self.over_default)
+        result = cli._should_show_limit_prompt(stmt, ['row']*self.over_default)
         assert cli.row_limit == 0
         assert not result
