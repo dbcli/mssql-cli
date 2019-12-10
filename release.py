3219e26dfd433605216e46b879e3edb8b8961948
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-12-10 12:43:03 -0800

    changed references to wheels

--- release.py
+++ release.py
@@ -100,13 +100,13 @@ def publish_official():
     """
     Publish mssql-cli package to PyPi.
     """
-    mssqlcli_wheel_dir = os.listdir(utility.MSSQLCLI_DIST_DIRECTORY)
+    mssqlcli_dist_dir = os.listdir(utility.MSSQLCLI_DIST_DIRECTORY)
     # Run twine action for mssql-cli.
     # Only authorized users with credentials will be able to upload this package.
     # Credentials will be stored in a .pypirc file.
-    for wheel in mssqlcli_wheel_dir:
+    for f in mssqlcli_dist_dir:
         utility.exec_command(
-            'twine upload {}'.format(wheel),
+            'twine upload {}'.format(f),
             utility.MSSQLCLI_DIST_DIRECTORY)
 
 
