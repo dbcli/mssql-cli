684e3f83278b3cc1438667b97879ba79425ce93d
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-12-16 21:20:13 -0800

    improved tests

--- tests/test_multiline.py
+++ tests/test_multiline.py
@@ -7,8 +7,10 @@ class TestMssqlCliMultiline:
         ('select 1 /* open comment!\ngo', False),
         ('select 1\ngo -- another comment', True),
         ('select 1; select 2, "open quote: go', False),
+        ('select 1; go', False),
         ('select 1\n select 2;\ngo', True),
-        ('select 1;', False)
+        ('select 1;', False),
+        ('select 3 /* another open comment\n*/   go', True)
     ]
 
     @staticmethod
