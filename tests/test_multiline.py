5ec6f294cc69c89e7982962fe98c001ecba4608f
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2020-01-09 15:00:42 -0800

    added test cases

--- tests/test_multiline.py
+++ tests/test_multiline.py
@@ -9,10 +9,13 @@ class TestMssqlCliMultiline:
         ('select 1 /* open comment!\ngo', False),
         ('select 1\ngo -- another comment', True),
         ('select 1; select 2, "open quote: go', False),
+        ('select 1\n"go"', False),
         ('select 1; GO', False),
+        ('SELECT 4;\nGO', True),
         ('select 1\n select 2;\ngo', True),
         ('select 1;', False),
-        ('select 1 go', False)
+        ('select 1 go', False),
+        ('select 1\ngo go go', False)
         # tests below to be enabled when sqlparse supports retaining newlines
         # when stripping comments:
         # ('select 3 /* another open comment\n*/   GO', True),
