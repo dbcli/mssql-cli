8f34d028c63d5c7c0db3dc35b3d3cf6a404ab5c2
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2020-02-20 11:01:36 -0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: SQL',
         'Topic :: Database',
         'Topic :: Database :: Front-Ends',
