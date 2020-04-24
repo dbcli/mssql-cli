36cba29cdc4eb3b87c20d25dcfbeadd638884bfc
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2020-04-24 16:23:39 -0700

    removed dev-latest

--- build_scripts/rpm/build.sh
+++ build_scripts/rpm/build.sh
@@ -26,6 +26,4 @@ rpmbuild -v -bb --clean ${REPO_PATH}/build_scripts/rpm/mssql-cli.spec
 # Copy build artifact to output dir.
 mkdir ${REPO_PATH}/../rpm_output
 cp ~/rpmbuild/RPMS/x86_64/*.rpm ${REPO_PATH}/../rpm_output
-# Create a second copy for latest dev version to be used by homepage.
-cp ~/rpmbuild/RPMS/x86_64/*.rpm ${REPO_PATH}/../rpm_output/mssql-cli-dev-latest.rpm
 echo "The archive has also been outputted to ${REPO_PATH}/../rpm_output"
