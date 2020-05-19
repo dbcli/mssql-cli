ee0b371a5d3f013bb81f87135dae26267c447071
chrisg123 <chrisg_123@hotmail.com>
2020-05-19 16:45:16 -0400

    (#401) re.error: bad escape \e #401

--- mssqlcli/packages/special/commands.py
+++ mssqlcli/packages/special/commands.py
@@ -314,7 +314,7 @@ def get_editor_query(sql):
     # The reason we can't simply do .strip('\e') is that it strips characters,
     # not a substring. So it'll strip "e" in the end of the sql also!
     # Ex: "select * from style\e" -> "select * from styl".
-    pattern = re.compile(r'(^\\\e|\\\e$)')
+    pattern = re.compile(r'(^\\e|\\e$)')
     while pattern.search(sql):
         sql = pattern.sub('', sql)
 
