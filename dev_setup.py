d9009fd1300911e88aedb4dbb589c4448dad783e
pensivebrian <pensivebrian@hotmail.com>
2021-01-28 15:49:59 -0800

    Change pip resolution in dev_setup.py

--- dev_setup.py
+++ dev_setup.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 from __future__ import print_function
+
 import os
+import sys
 import utility
 import mssqlcli.mssqltoolsservice.externals as mssqltoolsservice
 
-PIP = os.getenv('CUSTOM_PIP', 'pip')
+PYTHON = os.getenv('CUSTOM_PYTHON', sys.executable)
 
 print('Running dev setup...')
 print('Root directory \'%s\'\n' % utility.ROOT_DIR)
 
 # install general requirements.
-utility.exec_command('%s install --no-cache-dir -r requirements-dev.txt' % PIP,
+utility.exec_command('%s -m pip install --no-cache-dir -r requirements-dev.txt' % PYTHON,
                      utility.ROOT_DIR)
 
 
