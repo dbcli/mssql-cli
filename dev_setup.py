80c6df1b6b1907c3d584ad0b48f925f8df9911b9
Brian O'Neill <broneill@microsoft.com>
2021-01-28 17:47:38 -0800

    Upgrade to latest and download the SqlToolsService (#506)
    * Upgrade to latest sqltoolsservice
    * Change pip resolution in dev_setup.py
    * Download the sqltoolsservice platform specific packages from github

--- dev_setup.py
+++ dev_setup.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 from __future__ import print_function
+
 import os
+import sys
 import utility
 
-PIP = os.getenv('CUSTOM_PIP', 'pip')
+PYTHON = os.getenv('CUSTOM_PYTHON', sys.executable)
 
 print('Running dev setup...')
 print('Root directory \'%s\'\n' % utility.ROOT_DIR)
 
 # install general requirements.
-utility.exec_command('%s install --no-cache-dir -r requirements-dev.txt' % PIP,
+utility.exec_command('%s -m pip install --no-cache-dir -r requirements-dev.txt' % PYTHON,
                      utility.ROOT_DIR)
+
+import mssqlcli.mssqltoolsservice.externals as mssqltoolsservice
+
+# download the sqltoolssevice binaries for all platforms
+mssqltoolsservice.download_sqltoolsservice_binaries()
 
 # install mssqltoolsservice if this platform supports it.
 utility.copy_current_platform_mssqltoolsservice()
